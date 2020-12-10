b0a8e6dcbbdf514ebc75e9b9db0325a5c74ccac6
Julian Müller <julian.mueller@trustedshops.de>
2020-12-10 09:41:06 +0100

    Added encryption + made big sur compatible

--- Scripts/RemoveAllStaticGroupMemberships.sh
+++ Scripts/RemoveAllStaticGroupMemberships.sh
@@ -1,35 +1,51 @@
 #!/bin/zsh
-
-# Remove all static group memberships. Useful for wiping a device.
-
-APIUSER=$4
-APIPASS=$5
-
 setopt sh_word_split
 
-JSSURL="$(defaults read /Library/Preferences/com.jamfsoftware.jamf.plist jss_url)"
-GROUPS="$(curl -s -u "${APIUSER}:${APIPASS}" "${JSSURL}JSSResource/computers/udid/$(system_profiler SPHardwareDataType | awk '/UUID/ { print $3; }')/subset/GroupsAccounts" | xpath "/computer/groups_accounts/computer_group_memberships/group" 2>/dev/null | tidy -xml 2>/dev/null)"
-SERIAL="$(ioreg -c IOPlatformExpertDevice -d 2 | awk -F\" '/IOPlatformSerialNumber/{print $(NF-1)}')"
+APIUSER="$4"
+APIPASS="$5"
+
+ENCSALT="$6"
+ENCKEY="$7"
+
+if [[ -n $ENCKEY ]];then
+    APIPASS="$(echo "$APIPASS" | /usr/bin/openssl enc -aes256 -d -a -A -S "$ENCSALT" -k "$ENCKEY")"
+fi
+
+JSSURL="$(/usr/bin/defaults read /Library/Preferences/com.jamfsoftware.jamf.plist jss_url)"
+SERIAL="$(/usr/sbin/ioreg -c IOPlatformExpertDevice -d 2 | /usr/bin/awk -F\" '/IOPlatformSerialNumber/{print $(NF-1)}')"
+GROUPS="$(/usr/bin/curl -s -u "${APIUSER}:${APIPASS}" "${JSSURL}JSSResource/computers/serialnumber/${SERIAL}/subset/groups_accounts" | /usr/bin/xmllint --xpath "/computer/groups_accounts/computer_group_memberships" - 2>/dev/null)"
+
+if [[ -z $GROUPS ]];then
+	echo "Getting group memberships failed."
+    exit 1
+fi
 
 IFS=$'\n'
-for GROUP in $GROUPS;do
-    GROUP_FORMAT="$(echo "$GROUP" | awk -F'>' '{print $2}' | awk -F'<' '{print $1}')"
-    HTML_GROUP="$(echo "$GROUP_FORMAT" | sed 's/ /'%20'/g')"
-
-	echo -n "Processing $GROUP_FORMAT: "
-
-    GROUP_DATA="$(curl -s -u "${APIUSER}:${APIPASS}" "${JSSURL}JSSResource/computergroups/name/${HTML_GROUP}")"
+i=1
+while echo "$GROUPS" | /usr/bin/xmllint --xpath "/computer_group_memberships/group[$i]" - &>/dev/null;do
+    GROUP="$(echo "$GROUPS" | /usr/bin/xmllint --xpath "/computer_group_memberships/group[$i]/text()" -)"
+	echo -n "Processing $GROUP: "
     
-    SMART="$(echo $GROUP_DATA | xpath "/computer_group/is_smart" 2>/dev/null | awk -F'>' '{print $2}' | awk -F'<' '{print $1}')"
+    HTML_ENC_GROUP="$(echo "$GROUP" | /usr/bin/sed 's/ /'%20'/g')"
+    GROUP_DATA="$(/usr/bin/curl -s -u "${APIUSER}:${APIPASS}" "${JSSURL}JSSResource/computergroups/name/${HTML_ENC_GROUP}")"
+    
+    if [[ -z $GROUP_DATA ]];then
+    	echo "Could not receive group data."
+        exit 1
+    fi
+    
+    SMART="$(echo $GROUP_DATA | /usr/bin/xmllint --xpath "/computer_group/is_smart/text()" -)"
+
 
     if [[ "$SMART" == "false" ]];then
-    	echo "Static. Sending deletion request."
-        ID="$(echo $GROUP_DATA | xpath "/computer_group/id" 2>/dev/null | awk -F'>' '{print $2}' | awk -F'<' '{print $1}')"
-        DATA="<?xml version=\"1.0\" encoding=\"UTF-8\"?><computer_group><id>${ID}</id><name>${GROUP_FORMAT}</name><computer_deletions><computer><serial_number>${SERIAL}</serial_number></computer></computer_deletions></computer_group>"
-        DATA=$(echo "$DATA" | tidy -xml -utf8 -i 2>/dev/null)
-        curl -H "accept: application/xml" -H "Content-Type: application/xml" -u "${APIUSER}:${APIPASS}" "${JSSURL}JSSResource/computergroups/id/${ID}" -d "${DATA}" -X PUT -s
+    	echo "Sending deletion request."
+        ID="$(echo $GROUP_DATA | /usr/bin/xmllint --xpath "/computer_group/id/text()" -)"
+        DATA="<?xml version=\"1.0\" encoding=\"UTF-8\"?><computer_group><computer_deletions><computer><serial_number>${SERIAL}</serial_number></computer></computer_deletions></computer_group>"
+        /usr/bin/curl -H "accept: application/xml" -H "Content-Type: application/xml" -u "${APIUSER}:${APIPASS}" "${JSSURL}JSSResource/computergroups/id/${ID}" -d "${DATA}" -X PUT -s
     	echo ""
     else
-    	echo "Not static."
+    	echo "Skip."
     fi
+
+    i=$(($i+1))
 done
