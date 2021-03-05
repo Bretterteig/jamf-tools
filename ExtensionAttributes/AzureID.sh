0e1e89b71220ecb01c403c776c9f536fa56aca13
Bretterteig <julian.mueller@trustedshops.de>
2021-03-05 15:38:43 +0100

    Added AzureID

---
+++ ExtensionAttributes/AzureID.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# If Conditional Access is enabled via Company Portal (Workplace Join) display the Azure AAD ID of that device
+user="$(/usr/sbin/scutil <<<"show State:/Users/ConsoleUser" | /usr/bin/awk -F': ' '/[[:space:]]+Name[[:space:]]:/ { print $2 }')"
+
+echo "<result>$(su "$user" -c "security find-certificate -a -Z | grep -B 9 "MS-ORGANIZATION-ACCESS" | awk '/\"alis\"<blob>=\"/ {print $NF}' | sed 's/  \"alis\"<blob>=\"//;s/.$//'")</result>"
