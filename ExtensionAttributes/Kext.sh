4af89e40d6909ca8c799f42f37dd20d19ad55925
Bretterteig <julian.mueller@trustedshops.de>
2021-03-05 15:38:38 +0100

    Improved runtime and better display

--- ExtensionAttributes/Kext.sh
+++ ExtensionAttributes/Kext.sh
@@ -5,32 +5,27 @@ setopt sh_word_split
 IFS=$'\n'
 
 database_data="$(sqlite3 /var/db/SystemPolicyConfiguration/KextPolicy 'SELECT team_id,bundle_id,allowed FROM "kext_policy"')"
-if [[ -z $database_data ]];then
-    echo "<result>Could not read KextPolicy db or it is empty</result>"
-    exit 1
-fi
+[[ -z $database_data ]] && exit 0
 
 for line in $database_data;do
-    # Get service and program. 
-    teamid="$(echo "$line" | awk -F'|' '{print $1}')"
-    bundleid="$(echo "$line" | awk -F'|' '{print $2}')"
-    allowed="$(echo "$line" | awk -F'|' '{print $3}')"
+    data=("${(@s/|/)line}")
 
-    if [[ $allowed == "1" ]];then
+    teamid=$data[1]
+    bundleid=$data[2]
+
+    if [[ $data[3] == "1" ]];then
         allowed="GRANTED"
     else
         allowed="DENIED"
     fi
 
-    if [[ -n "$(/usr/bin/kmutil showloaded --list-only --bundle-identifier "$bundleid")" ]];then
+    if [[ -n "$(/usr/bin/kmutil showloaded --list-only --bundle-identifier "$bundleid" 2>/dev/null)" ]];then
         loaded="RUNNING"
     else
         loaded="INACTIVE"
     fi
-    # Build table data
-    table="[$allowed and $loaded] $bundleid (TID: $teamid)\n$table"
+
+    table=("[$allowed and $loaded] $bundleid ($teamid)" $table)
 done
 
-# Remove prefix and sort data so it is more readable.
-final_data="$(echo "$table" | sed s%kTCCService%% | sed s%SystemPolicy%% | sort | sed -E /^$/d)"
-echo "<result>$final_data</result>"
+echo "<result>$(printf '%s\n' "${table[@]}" | sort)</result>"
--- ExtensionAttributes/SystemExtensions.sh
+++ ExtensionAttributes/SystemExtensions.sh
@@ -1,2 +1,15 @@
 #!/bin/zsh
-echo "<result>$(/usr/bin/systemextensionsctl list | grep -vE '^enabled' | grep -v 'extension(s)' | cut -f-4)</result>"
+extensions=$(/usr/libexec/PlistBuddy -x -c 'print extensions' /Library/SystemExtensions/db.plist)
+
+i=0
+while data=$(/usr/libexec/PlistBuddy -x -c "print $i" /dev/stdin <<< $extensions 2>/dev/null);do
+    teamid="$(/usr/libexec/PlistBuddy -c 'print teamID' /dev/stdin <<< $data)"
+    identifier="$(/usr/libexec/PlistBuddy -c 'print identifier' /dev/stdin <<< $data)"
+    state="$(/usr/libexec/PlistBuddy -c 'print state' /dev/stdin <<< $data)"
+
+    table=("[${state:u}] $identifier ("$teamid")" $table)
+
+    i=$(($i+1))
+done
+
+echo "<result>$(printf '%s\n' "${table[@]}" | sort)</result>"
