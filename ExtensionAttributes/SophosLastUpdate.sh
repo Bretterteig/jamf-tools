ea0a5ec76df74154c0902db58e29ff9c891f0bec
Bretterteig <julian.mueller@trustedshops.de>
2021-03-31 15:10:03 +0200

    Added output TZ option

--- ExtensionAttributes/SophosLastUpdate.sh
+++ ExtensionAttributes/SophosLastUpdate.sh
@@ -2,4 +2,8 @@
 
 # Date of the last update
 # Configure as date extension attribute
+
+# Configure the timezone of the output
+export TZ="GMT-1"
+
 echo "<result>$(/bin/date -jf "%s" "$(defaults read /Library/Caches/com.sophos.sau/sophosautoupdate.plist LastTimeStamp)" "+%Y-%m-%d %H:%M:%S")</result>"
